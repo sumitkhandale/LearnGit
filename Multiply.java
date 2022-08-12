8f42517c430adcd3327c0931983eba7f8deb3a24
sumitk <sumitkhandale73@gmail.com>
2022-08-12 21:05:00 +0530

    adding multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+/**
+ * Multiply
+ */
+public class Multiply {
+
+    public static void main(String[] args) {
+        int n1 = 90,n2 = 10,n3;
+        n3 = n1 * n2;
+        System.out.println(n3);
+    }
+}
