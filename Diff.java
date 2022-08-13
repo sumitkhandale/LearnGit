2d7bd32d96c1063dcb4071846a89e1210fe69400
sumitk <sumitkhandale73@gmail.com>
2022-08-13 08:22:49 +0530

    Changes made on Diff.java

--- Diff.java
+++ Diff.java
@@ -4,7 +4,7 @@
 public class Diff {
 
     public static void main(String[] args) {
-        int n1 = 90,n2 = 10,n3;
+        int n1 = 100,n2 = 50,n3;
         n3 = n1 - n2;
         System.out.println(n3);
     }
